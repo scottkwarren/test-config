2fa33983babd80fb9ddaa17af4e02606d66fb103
Scott K. Warren <scott@rice.edu>
2017-08-09 17:01:13 -0500

    Refactor json loading.

--- scripts/load.py
+++ scripts/load.py
@@ -2,9 +2,26 @@ import json
 import os
 
 
-machine = json.load( os.popen("grep -v '^#' machines/bluebiou.json") )
+def load_json(kind, basename):
+    return json.load( os.popen("cpp -P -E {0}/{1}.json".format(kind, basename)) )
+
+
+class ConfigDB :
+    
+    def __init__(self, machineName, appName):
+        self.machine = load_json("machine", "bluebiou")
+        self.app     = load_json("app", "namd2")
+
+
+
+
+
+
+machine = load_json("machine", "bluebiou")
 
 print json.dumps(machine, sort_keys=True, indent=4)
 
 print "C compiler kinds: ", machine["c compiler"].keys()
 print "Gnu C compiler versions: ", machine["c compiler"]["gnu"].keys()
+
+
