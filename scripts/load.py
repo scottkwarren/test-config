0a73838b548766cdf560abe29fe4927d09e38d08
Scott K. Warren <scott@rice.edu>
2017-08-10 16:16:04 -0500

    Rough out app and package JSON formats.

--- scripts/load.py
+++ scripts/load.py
@@ -2,26 +2,36 @@ import json
 import os
 
 
-def load_json(kind, basename):
-    return json.load( os.popen("cpp -P -E {0}/{1}.json".format(kind, basename)) )
-
 
 class ConfigDB :
     
     def __init__(self, machineName, appName):
-        self.machine = load_json("machine", "bluebiou")
-        self.app     = load_json("app", "namd2")
+        self.loadMachine(machineName)
+        self.loadApp(appName)
+        self.loadPackages()
+
+    def loadMachine(self, machineName) :
+        self.machine = self.load_json("machines", machineName)
+
+    def loadApp(self, appName) :
+        self.app = self.load_json("apps", appName)
+
+    def loadPackages(self) :
+        self.pkgs = dict()
+        self.pkgs.update( self.load_json("packages", "slurm") )
+
+    def load_json(self, kind, basename) :
+        return json.load( os.popen("cpp -P -E {0}/{1}.json".format(kind, basename)) )
 
 
 
 
 
+db = ConfigDB("les", "namd2")
 
-machine = load_json("machine", "bluebiou")
+print json.dumps(db.machine, sort_keys=True, indent=4)
 
-print json.dumps(machine, sort_keys=True, indent=4)
-
-print "C compiler kinds: ", machine["c compiler"].keys()
-print "Gnu C compiler versions: ", machine["c compiler"]["gnu"].keys()
+print "C compiler kinds: ", db.machine["c compiler"].keys()
+print "Gnu C compiler versions: ", db.machine["c compiler"]["gnu"].keys()
 
 
